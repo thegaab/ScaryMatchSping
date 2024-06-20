830cf20de914c3a517cf1c18fe6fb9304e76092a
thegaab <tiagogabrielcontato@hotmail.com>
2024-06-20 13:48:35 -0300

    complete data search

--- src/main/java/br/com/alura/scarymatch/ScarymatchApplication.java
+++ src/main/java/br/com/alura/scarymatch/ScarymatchApplication.java
@@ -1,8 +1,6 @@
 package br.com.alura.scarymatch;
 
-import br.com.alura.scarymatch.model.DadosSerie;
-import br.com.alura.scarymatch.service.ConsumoAPI;
-import br.com.alura.scarymatch.service.ConverteDados;
+import br.com.alura.scarymatch.principal.Principal;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -16,11 +14,7 @@ public class ScarymatchApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		var consumoAPI = new ConsumoAPI();
-		var json = consumoAPI.obterDados("http://www.omdbapi.com/?t=gilmore+girls&apikey=a8619a59");
-		System.out.println(json);
-		ConverteDados conversor = new ConverteDados();
-		DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
-		System.out.println(dados);
+		Principal principal = new Principal();
+		principal.exibeMenu();
 	}
 }
---
+++ src/main/java/br/com/alura/scarymatch/principal/Principal.java
@@ -0,0 +1,39 @@
+package br.com.alura.scarymatch.principal;
+
+import br.com.alura.scarymatch.model.DadosSerie;
+import br.com.alura.scarymatch.model.DadosTemporada;
+import br.com.alura.scarymatch.service.ConsumoAPI;
+import br.com.alura.scarymatch.service.ConverteDados;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Principal {
+
+    private Scanner leitura = new Scanner(System.in);
+
+    private ConsumoAPI consumo = new ConsumoAPI();
+
+    private ConverteDados conversor = new ConverteDados();
+
+    private final String ENDERECO = "https://www.omdbapi.com/?t=";
+    private final String API_KEY = "&apikey=a8619a59";
+
+    public void exibeMenu(){
+        System.out.println("Digite o nome da série que deseja buscar: ");
+        var nomeSerie = leitura.nextLine();
+        var json = consumo.obterDados(ENDERECO + nomeSerie.replace(" ", "+") + API_KEY);
+        DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
+        System.out.println(dados);
+
+        List<DadosTemporada> temporadas =  new ArrayList<>();
+
+		for (int i = 1; i <=dados.totalTemporadas(); i++) {
+			json = consumo.obterDados(ENDERECO + nomeSerie.replace(" ", "+") + "&season=" + i + API_KEY);
+			DadosTemporada dadosTemporada = conversor.obterDados(json, DadosTemporada.class);
+			temporadas.add(dadosTemporada);
+		}
+		temporadas.forEach(System.out::println);
+    }
+}
